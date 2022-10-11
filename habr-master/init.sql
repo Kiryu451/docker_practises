8ced43762303c2af3e718de9abcf7aacb2cb6524
Kiryu451 <90781461+Kiryu451@users.noreply.github.com>
2022-10-11 20:52:17 +0300

    Delete habr-master directory

--- habr-master/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS appDB;
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-GRANT SELECT,UPDATE,INSERT ON appDB.* TO 'user'@'%';
-FLUSH PRIVILEGES;
-
-CREATE TABLE IF NOT EXISTS users (user varchar(200) not null, passwd varchar(200), primary key (user));
-INSERT INTO users VALUE ('Kiryu451', '{SHA}QL0AFWMIX8NRZTKeof9cXsvbvu8=');
--- habr-master/php/src/check.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = $_GET['psw'];
-$login= $_GET['uname'];
-    header("Location: http://{$login}:{$password}@localhost/private/home.php");
-    exit();
-?>
