5bcb6174b351fb6298cac395f22121f14c77a7be
Kiryu451 <90781461+Kiryu451@users.noreply.github.com>
2022-10-12 11:43:25 +0300

    Add files via upload

---
+++ PR3/php/src/private/home.php
@@ -0,0 +1,10 @@
+<html lang="ru">
+<head>
+  <meta charset="utf-8" />
+  <title></title>
+  <link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+    <a href="">Кароче у меня не работает стайл</a>   
+</body>
+</html>
